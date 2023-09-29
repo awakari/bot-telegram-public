ad3cf3fe33fb6326cdc1a9ec18c5f1bdb2599df8
Andrei Kurilov <18027129+akurilov@users.noreply.github.com>
2023-09-29 10:52:04 +0300

    save

--- docs/msg-new.js
+++ docs/msg-new.js
@@ -1,5 +1,5 @@
 templateMsgAttr = (name, type, value, required) => ` <span id="msg_attr_${name}" class="flex w-full text-xs font-mono h-[24x] min-h-[24px] items-center">
-                        <input type="text" id="msg_attr_${name}_" value="${name}" disabled="disabled" class="w-[64px] min-w-[64px] truncate border focus:shadow-md outline-none"/>
+                        <input type="text" id="msg_attr_${name}_" value="${name}" disabled="disabled" class="msg-attr-name w-[64px] min-w-[64px] truncate border focus:shadow-md outline-none"/>
                         <p id="msg_attr_${type}" class="w-[64px] min-w-[64px] px-1 truncate">${type}</p>
                         <input type="text" id="msg_attr_${value}" value="${value}" disabled="disabled" class="w-full font-mono truncate border focus:shadow-md outline-none text-slate-700"/>
                         ${required ? "" : "<button type=\"button\" title=\"Add Attribute\" onclick=\"deleteMessageAttribute('${name}');\" class=\"ml-1 text-2xl font-mono focus:outline-none text-slate-500 hover:text-slate-700 flex items-center justify-center h-[24x] max-h-[24px]\">—</button>"}
