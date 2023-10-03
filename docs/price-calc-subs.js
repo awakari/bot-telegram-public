71e5e20341cf856fa0966e9129ff0c1f9be42e40
Andrei Kurilov <18027129+akurilov@users.noreply.github.com>
2023-10-03 20:09:48 +0300

    save

--- docs/price-calc-subs.js
+++ docs/price-calc-subs.js
@@ -1,23 +1,18 @@
-const pricePerSub = 0.15;
+const pricePerSub = 0.1;
 const usageSubjSub = 1;
+const periodDays = 30;
 
 window.Telegram.WebApp.expand();
 
 function loadForm() {
+    document.getElementById("period_days").innerText = `${periodDays}`;
     let until = new Date();
-    until.setHours(until.getHours() + 168); // min is 7 days
-    document.getElementById("date_until").min = until.toISOString().split("T")[0];
-    until.setHours(until.getHours() + 24 * 23); // default is 30 days
-    document.getElementById("date_until").valueAsDate = until;
+    until.setHours(until.getHours() + 24 * periodDays); // default is 30 days
+    document.getElementById("date_until").innerText = until.toDateString();
     refreshPrice();
 }
 
 function refreshPrice(evtSrcId) {
-
-    let now = new Date();
-    let until = document.getElementById("date_until").valueAsDate;
-    let daysTotal = Math.ceil((until - now) / (1000*60*60*24));
-    document.getElementById("days_total").valueAsNumber = daysTotal;
 
     let evtSrc = document.getElementById(evtSrcId)
     switch (evtSrcId) {
@@ -31,7 +26,7 @@ function refreshPrice(evtSrcId) {
 
     let countLimitSubs = document.getElementById("count_limit_subs").valueAsNumber;
     document.getElementById("price_daily").innerText = `${(pricePerSub*(countLimitSubs - 1)).toFixed(2)}`;
-    document.getElementById("price_total").innerText = `${(daysTotal * pricePerSub*(countLimitSubs - 1)).toFixed(2)}`;
+    document.getElementById("price_total").innerText = `${(periodDays * pricePerSub*(countLimitSubs - 1)).toFixed(2)}`;
 }
 
 function submitMsg() {
