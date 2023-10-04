2bcf0261b986feebf67570652965c112ad3c09f6
Andrei Kurilov <18027129+akurilov@users.noreply.github.com>
2023-10-04 15:36:57 +0300

    save

---
+++ docs/price-calc-sub-extend.js
@@ -0,0 +1,37 @@
+const pricePerSub = 0.1;
+
+window.Telegram.WebApp.expand();
+
+function loadForm() {
+    document.getElementById("price_daily").innerText = `${pricePerSub}`;
+    refreshPrice();
+}
+
+function refreshPrice(evtSrcId) {
+
+    let evtSrc = document.getElementById(evtSrcId)
+    switch (evtSrcId) {
+        case "add_days":
+            document.getElementById("add_days_range").valueAsNumber = evtSrc.valueAsNumber;
+            break
+        case "add_days_range":
+            document.getElementById("add_days").valueAsNumber = evtSrc.valueAsNumber;
+            break
+    }
+    let addDays = document.getElementById("add_days").valueAsNumber;
+    document.getElementById("price_total").innerText = `${(addDays * pricePerSub).toFixed(2)}`;
+}
+
+function submitMsg() {
+    let addDays = document.getElementById("add_days").valueAsNumber;
+    const payload = {
+        "subExtend": {
+            "timeDays": addDays,
+        },
+        "price": {
+            "total": parseFloat(document.getElementById("price_total").innerText),
+            "unit": "EUR",
+        }
+    }
+    window.Telegram.WebApp.sendData(JSON.stringify(payload));
+}
