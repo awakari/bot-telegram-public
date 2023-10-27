1a89ed72ee3dbde4d4e9604ffd75e466991523fd
Andrei Kurilov <18027129+akurilov@users.noreply.github.com>
2023-10-27 11:59:54 +0300

    save

--- docs/source-add.js
+++ docs/source-add.js
@@ -1,19 +1,8 @@
-const minMsgCountDaily = 10;
-const pricePerMsgDaily = 0.04;
-const pricePerUpdDaily = 0.01;
-const periodDays = 30;
-
 window.Telegram.WebApp.expand();
 
 function loadForm() {
-    document.getElementById("period_days").innerText = `${periodDays}`;
-    let until = new Date();
-    until.setHours(until.getHours() + 24 * periodDays); // default is 30 days
-    document.getElementById("date_until").innerText = until.toDateString();
-    refreshPrice();
     document.getElementById("src_type").onchange = showSrcDetails;
     showSrcDetails();
-    document.getElementById("feed_upd_freq").onchange = refreshPrice;
 }
 
 function showSrcDetails() {
@@ -28,34 +17,9 @@ function showSrcDetails() {
             document.getElementById("feed").style.display = "grid";
             break
     }
-    refreshPrice("");
-}
-
-function refreshPrice(evtSrcId) {
-
-    let evtSrc = document.getElementById(evtSrcId)
-    switch (evtSrcId) {
-        case "count_limit_msgs_daily":
-            document.getElementById("count_limit_msgs_daily_range").valueAsNumber = evtSrc.valueAsNumber;
-            break
-        case "count_limit_msgs_daily_range":
-            document.getElementById("count_limit_msgs_daily").valueAsNumber = evtSrc.valueAsNumber;
-            break
-    }
-
-    let countLimitMsgsDaily = document.getElementById("count_limit_msgs_daily").valueAsNumber;
-    let srcType = document.getElementById("src_type").value;
-    let updatesDaily = 1;
-    if (srcType === "feed") {
-        updatesDaily = parseInt(document.getElementById("feed_upd_freq").value);
-    }
-    const pricePerDay = pricePerMsgDaily*(countLimitMsgsDaily - minMsgCountDaily) + pricePerUpdDaily*(updatesDaily - 1);
-    document.getElementById("price_daily").innerText = `${pricePerDay.toFixed(2)}`;
-    document.getElementById("price_total").innerText = `${(periodDays*pricePerDay).toFixed(2)}`;
 }
 
 function submitMsg() {
-    const limitMsgsDailyCount = document.getElementById("count_limit_msgs_daily").valueAsNumber;
     const srcType = document.getElementById("src_type").value;
     let srcAddr;
     switch (srcType) {
@@ -68,13 +32,7 @@ function submitMsg() {
     }
     const payload = {
         "limit": {
-            "time": periodDays,
-            "count": limitMsgsDailyCount,
             "freq": parseInt(document.getElementById("feed_upd_freq").value),
-        },
-        "price": {
-            "total": parseFloat(document.getElementById("price_total").innerText),
-            "unit": "EUR",
         },
         "src": {
             "addr": srcAddr,
