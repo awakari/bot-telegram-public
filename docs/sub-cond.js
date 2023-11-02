ff22059a37b308b62465a3e333082102b0050435
Andrei Kurilov <18027129+akurilov@users.noreply.github.com>
2023-11-02 21:00:33 +0200

    save

--- docs/sub-cond.js
+++ docs/sub-cond.js
@@ -11,13 +11,13 @@ editor.on('change', function () {
 
     // Not valid
     if (errors.length) {
-        indicator.className = 'label label-danger'
-        indicator.textContent = "not valid";
+        document.getElementById("btn_submit").disabled = true;
+        document.getElementById("btn_submit_txt").innerText = "Not valid";
     }
     // Valid
     else {
-        indicator.className = 'label label-success'
-        indicator.textContent = "valid";
+        document.getElementById("btn_submit").disabled = false;
+        document.getElementById("btn_submit_txt").innerText = "Submit";
     }
 });
 
